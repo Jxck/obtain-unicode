389cad4e0f209b239fdd87c2b93ad72f515748ce
Jxck <block.rxckin.beats@gmail.com>
2015-01-22 23:02:32 +0900

    add test

--- test-obtain-unicode.js
+++ test-obtain-unicode.js
@@ -1,4 +1,10 @@
 var obtain = require('./obtain-unicode');
 
-var s = "あ";
+var n = "09azAZあ𠮟";
 console.log(obtain(s));
+
+var s = "𠮟";
+console.log(obtain(s));
+
+var n = "🍻";
+console.log(obtain(n));
